27dd835482120c0c8bb84b4252615ee29da213c4
Quezia Menezes <queziamenezessouza@gmail.com>
2023-02-02 18:11:58 -0300

    :rocket: Introduzindo os componentes da aplicação.

---
+++ NumerosAleatorios.js
@@ -0,0 +1,29 @@
+// import React from 'react';
+// import {Text} from 'react-native';
+
+// export default props => {
+//     const delta = props.max - props.min + 1
+//     const aleatorio = parseInt(Math.random() * delta) + props.min
+//     return (
+//         <Text>
+//             Número aleatório: {aleatorio}
+//         </Text>
+//     )
+// }
+
+// {/* ex <Aleatorio min={1} max ={100} /> */}
+
+// import React from 'react';
+// import {Text} from 'react-native';
+
+// export default ({ min, max }) => {
+//     const delta = max - min + 1
+//     const aleatorio = parseInt(Math.random() * delta) + props.min
+//     return (
+//         <Text>
+//             Número aleatório: {aleatorio}
+//         </Text>
+//     )
+// }
+
+// {/* ex <Aleatorio min={1} max ={100} /> */}
